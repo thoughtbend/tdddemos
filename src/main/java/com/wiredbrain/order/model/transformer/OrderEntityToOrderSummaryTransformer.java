7c20e4784b5c28d4efc4693342bb8e63c9d8d2d9
Mike Nolan <michael.nolan@parivedasolutions.com>
2019-05-03 17:03:19 -0500

    Update to Java Lambda

--- src/main/java/com/wiredbrain/order/model/transformer/OrderEntityToOrderSummaryTransformer.java
+++ src/main/java/com/wiredbrain/order/model/transformer/OrderEntityToOrderSummaryTransformer.java
@@ -6,35 +6,35 @@ import com.wiredbrain.order.model.domain.OrderSummary;
 import com.wiredbrain.order.model.entity.OrderEntity;
 import com.wiredbrain.order.model.entity.OrderItemEntity;
 
-
-
 public class OrderEntityToOrderSummaryTransformer {
 
 	public OrderSummary transform(OrderEntity orderEntity) {
-		
+
 		if (orderEntity == null) {
 			throw new IllegalArgumentException("orderEntity should not be null");
 		}
-		
+
 		OrderSummary orderSummaryResult = new OrderSummary();
-		
+
 		orderSummaryResult.setOrderNumber(orderEntity.getOrderNumber());
-		
-		int itemCount = 0;
-		BigDecimal totalAmount = new BigDecimal("0.00");
-		
-		for (OrderItemEntity currentItem : orderEntity.getOrderItemList()) {
-			
-			itemCount += currentItem.getQuantity();
-			
-			BigDecimal quantityBD = new BigDecimal(currentItem.getQuantity());
-			BigDecimal itemTotal = currentItem.getSellingPrice().multiply(quantityBD);
-			totalAmount = totalAmount.add(itemTotal);
-		}
-		
+
+		int itemCount = orderEntity.getOrderItemList().stream()
+									.mapToInt(OrderItemEntity::getQuantity)
+									.sum();
+
+		BigDecimal totalAmount = orderEntity.getOrderItemList().stream().map(orderItem -> {
+
+			BigDecimal quantityBD = new BigDecimal(orderItem.getQuantity());
+			BigDecimal itemTotal = orderItem.getSellingPrice().multiply(quantityBD);
+			return itemTotal;
+		}).reduce(new BigDecimal("0.00"), (left, right) -> {
+
+			return left.add(right);
+		});
+
 		orderSummaryResult.setItemCount(itemCount);
 		orderSummaryResult.setTotalAmount(totalAmount);
-		
+
 		return orderSummaryResult;
 	}
 }
